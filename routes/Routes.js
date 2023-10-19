060a34621199f2310e7fefb2c7986734cfb5b9ce
Kapil Chauhan <kapilchn7@gmail.com>
2023-10-19 18:50:44 +0530

    Commiting

--- routes/Routes.js
+++ routes/Routes.js
@@ -6,7 +6,10 @@ router.get("/query", (req, res) => {
   if (!req.query) {
     res.send(400);
   } else {
-    console.log("this is the request data ", req.query.query);
+    console.log(
+      "this is the request data, what could change ",
+      req.query.query
+    );
   }
 });
 export default router;
